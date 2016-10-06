f71992217a754ff7887079e20eb11ac089ff655d
user <user@6.1.1.1>
2016-10-07 01:44:46 +0800

    第一個程式碼

---
+++ src/word.java
@@ -0,0 +1,9 @@
+
+public class word {
+
+	public static void main(String[] args) {
+				String word="let it go! let it go! Cannot hold it back anymore";
+				word=word.replace("Cannot", "Can't");
+				System.out.println(word.replace("it", "\"her\""));
+	}
+}
